60c1579d010697f56f750e1e3e3428bcf726ece2
Daisuke TAKEUCHI <takenoco82@gmail.com>
2019-08-01 23:27:51 +0900

    [fix] globalなgitconfigを参照できるようにした

--- scripts/install-dev-tools.sh
+++ scripts/install-dev-tools.sh
@@ -11,6 +11,14 @@ cp -r ~/localhost_home/.ssh/* ~/.ssh
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/*
 
+# copy git config into container works
+cp ~/localhost_home/.gitconfig ~/
+if [[ -f ~/localhost_home/.gitconfig.local ]]; then
+  cp ~/localhost_home/.gitconfig.local ~/
+fi
+if [[ -f ~/localhost_home/.gitignore_global ]]; then
+  cp ~/localhost_home/.gitignore_global ~/
+fi
 
 # configure your custom settings
 sh user-settings.sh
