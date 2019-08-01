16a838fdb64535b5d81a2080d4952d5189769ab8
Daisuke TAKEUCHI <takenoco82@gmail.com>
2019-08-01 23:27:21 +0900

    [fix] ssh接続できるようにした

--- scripts/install-dev-tools.sh
+++ scripts/install-dev-tools.sh
@@ -3,7 +3,14 @@
 set -x
 
 # install devlopment tools
-apk add git zsh vim curl jq
+apk add git zsh vim curl jq openssh
+
+# copy ssh config into container works
+mkdir -p ~/.ssh
+cp -r ~/localhost_home/.ssh/* ~/.ssh
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/*
+
 
 # configure your custom settings
 sh user-settings.sh
